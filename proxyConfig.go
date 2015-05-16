465549e863e040780c583c57961fd5f8e2922a4e
Knut Ahlers <knut@ahlers.me>
2015-05-16 18:13:44 +0200

    Added yaml field definition

--- proxyConfig.go
+++ proxyConfig.go
@@ -9,27 +9,27 @@ import (
 )
 
 type proxyConfig struct {
-	Domains     map[string]domainConfig `json:"domains"`
-	Generic     string                  `json:"generic"`
-	Docker      dockerConfig            `json:"docker"`
-	ListenHTTP  string                  `json:"listenHTTP"`
-	ListenHTTPS string                  `json:"listenHTTPS"`
+	Domains     map[string]domainConfig `json:"domains" yaml:"domains"`
+	Generic     string                  `json:"generic" yaml:"generic"`
+	Docker      dockerConfig            `json:"docker" yaml:"docker"`
+	ListenHTTP  string                  `json:"listenHTTP" yaml:"listenHTTP"`
+	ListenHTTPS string                  `json:"listenHTTPS" yaml:"listenHTTPS"`
 }
 
 type domainConfig struct {
-	SSL      sslConfig `json:"ssl,omitempty"`
-	Slug     string    `json:"slug"`
-	ForceSSL bool      `json:"force_ssl"`
+	SSL      sslConfig `json:"ssl,omitempty" yaml:"ssl,omitempty"`
+	Slug     string    `json:"slug" yaml:"slug"`
+	ForceSSL bool      `json:"force_ssl" yaml:"force_ssl"`
 }
 
 type sslConfig struct {
-	Cert string `json:"cert"`
-	Key  string `json:"key"`
+	Cert string `json:"cert" yaml:"cert"`
+	Key  string `json:"key" yaml:"key"`
 }
 
 type dockerConfig struct {
-	Hosts map[string]string `json:"hosts"`
-	Port  int               `json:"port"`
+	Hosts map[string]string `json:"hosts" yaml:"hosts"`
+	Port  int               `json:"port" yaml:"port"`
 }
 
 func newProxyConfig(configFile string) (*proxyConfig, error) {
